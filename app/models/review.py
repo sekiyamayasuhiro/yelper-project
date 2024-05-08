802a831fa2c49514e39582969fb69fd96be002ed
hazelcaling <hazelcaling@live.com>
2024-05-07 23:21:22 -0700

    completed display_all_businesses route in the backend, updated models I added to_dict method, updated backend_routes.md I added response body for get all businesses

--- app/models/review.py
+++ app/models/review.py
@@ -11,11 +11,21 @@ class Review(db.Model):
     business_id = db.Column(db.Integer, db.ForeignKey('businesses.id'), nullable=False)
     rating = db.Column(db.Integer)
     review_text = db.Column(db.String(100))
-    # created_at = db.Column(db.DateTime, default=datetime.now)
-    # updated_at = db.Column(db.Datetime, default=datetime.now)
     created_at = db.Column(db.TIMESTAMP, default=db.func.current_timestamp())
     updated_at = db.Column(db.TIMESTAMP, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
 
     # Relationships
     user = db.relationship('User', back_populates='reviews')
     business = db.relationship('Business', back_populates='reviews')
+
+
+    def to_dict(self):
+        return  {
+            'id': self.id,
+            'user_id': self.user_id,
+            'business_id': self.business_id,
+            'rating': self.rating,
+            'review_text': self.review_text,
+            'created_at': self.created_at,
+            'updated_at': self.updated_at
+        }
