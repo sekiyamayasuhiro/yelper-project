60bc277de1ac8289d3e6b629e2b30508d080b517
hazelcaling <hazelcaling@live.com>
2024-05-08 23:31:31 -0700

    finished delete and get details of business by id, updated business model I added cascade delete for associated reviews and images

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -1,11 +1,39 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, abort
 from app.models import Business
+from app.models import db
+from flask_login import current_user
 
 business_routes = Blueprint('businesses', __name__)
 
 
 @business_routes.route('/')
-def display_all_businesses():
+def get_all_businesses():
     data = Business.query.all()
     businesses = [business.to_dict() for business in data]
     return {'Businesses': businesses}
+
+
+@business_routes.route('/<int:business_id>')
+def get_business(business_id):
+    business = Business.query.get(business_id)
+    if business:
+        return business.to_dict()
+    else:
+        abort(404)
+
+
+@business_routes.route('/<int:business_id>', methods=['DELETE'])
+def delete_business(business_id):
+    business = Business.query.get(business_id)
+
+    if not business:
+        abort(404)
+
+    isAuthorized = current_user.id == business.owner_id
+
+    if isAuthorized:
+        db.session.delete(business)
+        db.session.commit()
+        return {'message': 'Successfully deleted'}
+    else:
+        abort(403)
