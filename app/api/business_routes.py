7e5d7864713dd4b5fb47c110ad19943e9c37e560
hazelcaling <hazelcaling@live.com>
2024-05-09 20:29:57 -0700

    updated get business by id, I added to eager load associated reviews and iamges

--- app/api/business_routes.py
+++ app/api/business_routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, jsonify, abort
-from app.models import Business
+from app.models import Business, Image, Review
 from app.models import db
 from flask_login import current_user
+from sqlalchemy.orm import joinedload
 
 business_routes = Blueprint('businesses', __name__)
 
@@ -15,11 +16,22 @@ def get_all_businesses():
 
 @business_routes.route('/<int:business_id>')
 def get_business(business_id):
-    business = Business.query.get(business_id)
-    if business:
-        return business.to_dict()
-    else:
+    data = Business.query.options(joinedload(Business.images), joinedload(Business.reviews)).get(business_id)
+
+    if not data:
         abort(404)
+
+    business = data.to_dict()
+    images = [image.to_dict() for image in data.images]
+    reviews = [review.to_dict() for review in data.reviews]
+
+    res = {
+        **business,
+        'BusinessImages': images,
+        'BusinessReviews': reviews
+    }
+
+    return jsonify(res)
 
 
 @business_routes.route('/<int:business_id>', methods=['DELETE'])
