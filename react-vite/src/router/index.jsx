a680dc575ae2e3b08b5c78a2f8c121220914d36e
ThiPhucThinh <thiphucthinhhuynh@gmail.com>
2024-05-13 23:23:52 -0700

    Add the router

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,7 +1,14 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, Outlet } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
 import Layout from './Layout';
+import BusinessesIndex from '../components/BusinessesIndex';
+import CreateBusinessForm from '../components/CreateBusinessForm';
+import ManageBusinesses from '../components/ManageBusinesses';
+import BusinessDetails from '../components/BusinessDetails';
+import UpdateBusinessForm from '../components/UpdateBusinessForm';
+import ManageReviews from '../components/ManageReviews';
+
 
 export const router = createBrowserRouter([
   {
@@ -9,7 +16,33 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/",
-        element: <h1>Welcome!</h1>,
+        element: <BusinessesIndex />,
+      },
+      {
+        path: "businesses",
+        element: <Outlet />,
+        children: [
+          {
+            path: "new",
+            element: <CreateBusinessForm />
+          },
+          {
+            path: "current",
+            element: <ManageBusinesses />
+          },
+          {
+            path: ":businessId",
+            element: <BusinessDetails />
+          },
+          {
+            path: ":businessId/edit",
+            element: <UpdateBusinessForm />
+          }
+        ]
+      },
+      {
+        path: "reviews/current",
+        element: <ManageReviews />
       },
       {
         path: "login",
