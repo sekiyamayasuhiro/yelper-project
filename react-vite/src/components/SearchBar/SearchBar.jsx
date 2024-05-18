d933f7ea205eb21e9ab1bd75b7e0c96a3352c103
hazelcaling <hazelcaling@live.com>
2024-05-18 08:32:57 -0700

    Updated searchbar to redirect the user to the landing page if they are not already there, after redirection landing page displays filtered result, also I added demo user in the login modal

--- react-vite/src/components/SearchBar/SearchBar.jsx
+++ react-vite/src/components/SearchBar/SearchBar.jsx
@@ -2,29 +2,34 @@ import { useState } from 'react'
 import './SearchBar.css'
 import { useDispatch } from 'react-redux'
 import { getAllBusinesses} from '../../redux/business'
+import { useNavigate } from 'react-router-dom'
+
 
 
 function SearchBar() {
     const dispatch = useDispatch()
+    const navigate = useNavigate()
 
     const [name, setName] = useState('')
     const [price, setPrice] = useState('')
     const [category, setCategory] = useState('')
 
-    const prices = ['$', '$$', '$$$', '$$$$']
+    const prices = {1: '$', 2: '$$', 3: '$$$', 4: '$$$$'}
     const categories = ['Restaurant', 'Coffee', 'Gym', 'Salon']
 
     const handleToggle = (type, value) => {
+        navigate('/')
         if (type === 'category') {
             setCategory((prevCategory) => (prevCategory === value ? '' : value));
         } else if (type === 'price') {
-            setPrice(prevPrice => (prevPrice === value ? '' : value))
+            setPrice(prevPrice => (prevPrice === +value ? '' : +value))
         }
     }
 
     const handleSubmit =  (e) => {
         e.preventDefault()
-        dispatch(getAllBusinesses({name, price, category}))
+        navigate('/')
+        dispatch(getAllBusinesses({name, price, category}, navigate))
     }
 
     return (
@@ -44,8 +49,8 @@ function SearchBar() {
                         <span>
                             Price
                             <div className='price'>
-                            {prices.map((price, index) => (
-                            <button key={index} value={price} onClick={() => handleToggle('price', price)}>{price}</button>
+                            {Object.entries(prices).map(([key, val]) => (
+                            <button key={key} value={key} onClick={() => handleToggle('price', key)}>{val}</button>
                             ))}
                             </div>
                         </span>
@@ -53,7 +58,6 @@ function SearchBar() {
 
                 </div>
                 <button>Search</button>
-                {/* { businesses !== null || businesses !== undefined && Object.keys(businesses).length === 0 && 'No result'} */}
             </form>
         </div>
 
