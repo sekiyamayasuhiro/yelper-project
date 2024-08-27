892cb3cd21c68afee9ac2b4a5ec4d29d086641a5
hazelcaling <hazelcaling@live.com>
2024-08-26 19:33:05 -0700

    homepage/Add category suggestion grid with search functionality

--- react-vite/src/components/HomePage/HomePage.jsx
+++ react-vite/src/components/HomePage/HomePage.jsx
@@ -8,12 +8,13 @@ import {
     FaDumbbell,
     FaCut
 } from "react-icons/fa";
-
+import { useNavigate } from "react-router-dom";
 import './HomePage.css';
 
 export default function HomePage() {
+    const navigate = useNavigate()
     const categoryIcons = {
-        'Restaurants': <FaUtensils />,
+        'Restaurant': <FaUtensils />,
         'Shopping': <FaShoppingBag />,
         'Active Life': <FaBiking />,
         'Automotive': <FaCar />,
@@ -22,6 +23,12 @@ export default function HomePage() {
         'Gym': <FaDumbbell />,
         'Salon': <FaCut />,
     };
+
+    const handleClick = (cat) => {
+        navigate(`/businesses?category=${cat}`)
+
+
+    }
 
     return (
         <div className="homepage-container">
@@ -34,7 +41,7 @@ export default function HomePage() {
                 <h2>Categories</h2>
                 <div className="categories-container">
                     {Object.keys(categoryIcons).map((cat, index) => (
-                        <div key={index} className="category-card">
+                        <div key={index} className="category-card" onClick={() => handleClick(cat)}>
                             <div className="icon">{categoryIcons[cat]}</div>
                             <div className="category-name">{cat}</div>
                         </div>
