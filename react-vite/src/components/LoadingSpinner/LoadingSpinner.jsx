b01d02c3accaf3b371a170c799f8e17d93b6876d
hazelcaling <hazelcaling@live.com>
2024-09-04 17:22:07 -0700

    feat(loading-spinner): add spinner component and styling

---
+++ react-vite/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,12 @@
+import './LoadingSpinner.css';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner-container">
+      <div className="spinner"></div>
+      <p>Loading...</p>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
